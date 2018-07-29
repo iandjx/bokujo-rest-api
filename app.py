022f91e27d0600e4db4bb92a0281d5c48ae5a5a7
ian de jesus <ian.dejesus.md@gmail.com>
2018-07-29 14:44:17 +0900

    Added PostgreSQL environment variable and psycopg2

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 # from flask_jwt import JWT
@@ -6,7 +8,8 @@ from resources.vaccine import Vaccine
 # from.resources.artificialinsemination import ArtificialInsemination
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+# Get database URL from Heroku if available else use sqlite
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
